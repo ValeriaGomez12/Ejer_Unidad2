aad48d2142f644d1b9f77612750c70d60cd1308f
ValeriaGomez12 <79612118+ValeriaGomez12@users.noreply.github.com>
2022-10-18 09:01:38 -0500

    Update ejer.py

--- Unidad 2/ejer.py
+++ Unidad 2/ejer.py
@@ -33,7 +33,7 @@ Why did the list and dictionary contents not print?
 Fix the code and run the script again.
 """
 
-#print( capitals["South Africa"[1]] )
+print( capitals["South Africa"[0]] )
 """
 Why did you get an error for the 2nd capital of South Africa?
 Hint: Check the syntax for the index value.
