6d65841679cf6e443fe8e41ca77963308303022d
ValeriaGomez12 <79612118+ValeriaGomez12@users.noreply.github.com>
2022-10-18 09:21:18 -0500

    Add files via upload

---
+++ Unidad 2/03_personal-info.py
@@ -0,0 +1,15 @@
+'''
+Desscripccion: scrip para pedir datos
+Autor Cinthia Valeria Gomez Luna
+fecha 18 de septiembre del 2022
+
+'''
+print("¿Cómo se llama?")
+nombre = input()
+print(f"Me alegro de conocerle, {nombre}")
+
+print("¿Que edad tiene")
+edad = input()
+print()
+
+    
