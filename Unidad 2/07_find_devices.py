cf62bb62e5eff0ed17f7f55782568bd530b01559
ValeriaGomez12 <79612118+ValeriaGomez12@users.noreply.github.com>
2022-10-20 08:57:03 -0500

    Update 07_find_devices.py

--- Unidad 2/07_find_devices.py
+++ Unidad 2/07_find_devices.py
@@ -5,12 +5,15 @@ Fecha: 20 de octubre del 2022
 
 file = open("devices.txt", "r")
 dispo = input ("Debes ingresar el dispositivo que deseas ")
+isFound = False
 
 
 for line in file:
     line = line.strip()
-    if "Switch" in line:
+    if dispo in line and len(dispo) > 3:
+        isFound = True
         print(line)
+    
     else:
         
         print("tu dispositivo no se encontro")
