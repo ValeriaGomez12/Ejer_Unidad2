1572c35aecacfd9ee5e3d82d4fe24835ccd1cb28
ValeriaGomez12 <79612118+ValeriaGomez12@users.noreply.github.com>
2022-10-27 09:53:00 -0500

    Add files via upload

---
+++ Unidad 2/06_while-loop.py
@@ -0,0 +1,17 @@
+'''
+Desscripccion: scrip para pedir datos
+Autor Cinthia Valeria Gomez Luna
+fecha 18 de Octubre del 2022
+
+'''
+
+x= input("se debera ingresar un numero")
+x=int(x)
+y=1
+
+y = 1
+while y <= 10:
+    print(y)
+    y =y 
+    1
+    
---
+++ Unidad 2/archivos.py
@@ -0,0 +1,11 @@
+file = open("devices.txt", "r")
+devices = []
+
+for line in file:
+    line = line.strip()
+    devices.append(line)
+print(devices)
+file.close()
+
+
+    
