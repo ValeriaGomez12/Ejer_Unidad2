dfb88b00d26818f7d844b9647afbb2df7217764f
ValeriaGomez12 <79612118+ValeriaGomez12@users.noreply.github.com>
2022-11-03 10:30:38 -0600

    Add files via upload

---
+++ Unidad 2/06_while-loop.py
@@ -0,0 +1,13 @@
+'''
+Desscripccion: scrip para pedir datos
+Autor Cinthia Valeria Gomez Luna
+fecha 18 de Octubre del 2022
+
+'''
+
+x=input("Se debera Introducir un Numero: ")
+x=int(-x)
+y=1
+while y>=x:
+    print(y)
+    y=y+1
