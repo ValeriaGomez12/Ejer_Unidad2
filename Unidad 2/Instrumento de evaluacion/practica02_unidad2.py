da609f1d814d5f032f874ef704135637bbf72dca
ValeriaGomez12 <79612118+ValeriaGomez12@users.noreply.github.com>
2022-11-08 09:44:07 -0600

    Add files via upload

---
+++ Unidad 2/Instrumento de evaluacion/practica02_unidad2.py
@@ -0,0 +1,14 @@
+'''
+Nombre de la api Random facts api 
+Descripción de la API
+API Portal / Home Page
+'''
+
+from urllib.parse import urlparse
+import requests
+import json
+main_api = requests.get('')
+print("/n")
+print("")
+r = input()
+data =  main_api.json()
