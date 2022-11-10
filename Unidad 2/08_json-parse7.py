c5b74443e8ea0710ed529ee7c71504818b0811f6
ValeriaGomez12 <79612118+ValeriaGomez12@users.noreply.github.com>
2022-11-10 09:55:36 -0600

    Update 08_json-parse7.py

--- Unidad 2/08_json-parse7.py
+++ Unidad 2/08_json-parse7.py
@@ -1,40 +1,30 @@
-'''
-Autor: Cinthia Valeria Gomez Luna
-Fecha: 25 de octubre del 2022
-'''
-
-import json
-#08_parse-json1.py
 import urllib.parse
 import requests
-from pip import main
+import json
 
-#create variables for API request 
 main_api = "http://www.mapquestapi.com/directions/v2/route?"
-orig = "Washington"
-dest = "Baltimaore"
+address = ''
 key = "YrYI0yjA7ocl5gcgBEB551g3lqynmnHh"
-url = main_api + urllib.parse.urlencode ({"key":key, "from":orig, "to": dest})
 
-#create the json request 
-json_data = requests.get (url).json()
+url = main_api + urllib.parse.urlencode({'address':address}) + '&key=' + key
+
+json_data = requests.get(url).json()
 print(json_data)
 
-#08_json.parse2.py print the url
-print("url":  + (url))
+print("URL: " + (url))
+
 json_data = requests.get(url).json()
 json_status = json_data["info"] ["statuscode"]
+
 if json_status == 0:
     print("API Status: " + str(json_status) + "= A successful route call.\n")
 
-#add user input for adress 08:json-parse3.py
 while True:
     orig = input("Starting Location: ")
     dest = input("Destination: ")
     url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
-    print("url" + (url))
+    print("URL: " + (url))
 
-# add quit funtionality 08_json-parse4.py
 while True:
     orig = input("Starting Location: ")
     if orig == "quit" or orig == "q":
@@ -43,7 +33,6 @@ while True:
     if dest == "quit" or dest == "q":
         break
     
-    # 08_jsonparse5.py. parse and display trip data 
 if json_status == 0:
     print("API Status: " + str(json_status) + " = A successful route call.\n") 
     print("Directions from " + (orig) + " to " + (dest))
@@ -52,21 +41,18 @@ if json_status == 0:
     print("Fuel(Gal):       " + str(json_data["route"]["fuelUsed"])) 
     print("=============================================")
 
-#Convert imperial to metric
 print("Kilometers:      " + str((json_data["route"]["distance"])*1.61))
 print("Fuel Used (Ltr): " + str((json_data["route"]["fuelUsed"])*3.78))
 
-# Format to 2 decimal places 
 print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
 print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
 
-#08_json-parse6.py lterate through the directions data 08_json-parse6.py
 print("=============================================")
 for each in json_data["route"]["legs"][0]["maneuvers"]:
         print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
 print("=============================================\n")
 
-#08_json-parse7.py Check for invalid user input 
+
 if json_status == 0:{
     
 }
@@ -77,8 +63,7 @@ elif json_status == 402:
         print("For Staus Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
         print("**********************************************\n")
         print("=============================================")
-        
-#json_status
+
 elif json_status == 402:
         print("**********************************************")
         print("For Staus Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
@@ -89,4 +74,3 @@ else:
         print("For Staus Code: " + str(json_status) + ", Refer to:")
         print("https://developer.mapquest.com/documentation/directions-api/status-codes")
         print("************************************************************************\n")
-
