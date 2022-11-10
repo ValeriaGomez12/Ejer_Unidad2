ed16683eb83e9b4761d07bbc2a428f4f6d42c35b
ValeriaGomez12 <79612118+ValeriaGomez12@users.noreply.github.com>
2022-11-10 10:23:49 -0600

    Update 08_json-parse7.py

--- Unidad 2/08_json-parse7.py
+++ Unidad 2/08_json-parse7.py
@@ -1,20 +1,31 @@
 import urllib.parse
+#Se deberan importar los modulos existentes
 import requests
+#main_api esta debera ser la url a la cual estara accediendo
+
 import json
+#se importara el json 
 
 main_api = "http://www.mapquestapi.com/directions/v2/route?"
+# este debera ser el  parámetro para punto de origen'''
 address = ''
 key = "YrYI0yjA7ocl5gcgBEB551g3lqynmnHh"
+#la llave que creamos 
 
 url = main_api + urllib.parse.urlencode({'address':address}) + '&key=' + key
+#se debera crear una variable que usando el método get
 
 json_data = requests.get(url).json()
 print(json_data)
-
+#deber imprimir los datos del json 
+ 
+ 
 print("URL: " + (url))
+#tendra que imprimir la url 
 
 json_data = requests.get(url).json()
 json_status = json_data["info"] ["statuscode"]
+#debra imprimir el estado de la solicitud 
 
 if json_status == 0:
     print("API Status: " + str(json_status) + "= A successful route call.\n")
