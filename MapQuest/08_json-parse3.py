a1e73c4ea46e1236012698c1e3777b8a1cd17eaa
ValeriaGomez12 <79612118+ValeriaGomez12@users.noreply.github.com>
2022-11-15 21:43:37 -0600

    Update 08_json-parse3.py

--- MapQuest/08_json-parse3.py
+++ MapQuest/08_json-parse3.py
@@ -12,16 +12,20 @@ import requests
 #este debera ser el  parámetro para punto de origen
 main_api = "http://www.mapquestapi.com/directions/v2/route?"
 
+#nuestra llave  que sera clave del consumidor de la API 
 key  = "YrYI0yjA7ocl5gcgBEB551g3lqynmnHh"
 
+# Beberas escribir el origen y el destino para asi poderlos agregar a un ciclo while 
 while True:
     orig = input("Starting Location: ")
     dest = input("Destination: ")
     url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
     print("URL: " + (url))
     
+    #Se debera imprimir el json
     json_data = requests.get(url).json()
     json_status = json_data["info"]["statuscode"]
-
+    
+#e este apartado se imprimira 
     if json_status == 0:
        print("API Status: " + str(json_status) + " = A successful route call.\n")
